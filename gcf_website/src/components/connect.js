76ba56f7432054e3b9a3064011e9ffd50d931833
James <yl2vv@virginia.edu>
2021-01-18 02:35:50 -0500

    connect page

--- gcf_website/src/components/connect.js
+++ gcf_website/src/components/connect.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./connect.css";
 import {FaFacebook, FaYoutube, FaTwitter, FaInstagram} from "react-icons/fa";
+import {HiOutlineMail} from "react-icons/hi";
+import Fade from 'react-reveal/Fade';
 
 function Connect() {
   return (
@@ -8,20 +10,40 @@ function Connect() {
       <div className='banner'>
        <p className="connect_title">Connect with us</p>
       </div>
-      <div className="socialMedia">
-        <p>Find us on social media</p>
-        <a href="https://www.facebook.com/GCFatUVA" target="_blank" rel="noreferrer" className="social">
-            <FaFacebook/>
-          </a>
-          <a href="https://www.instagram.com/gcfuva/" target="_blank" rel="noreferrer" className="social">
-            <FaInstagram />
-          </a>
-        <a href="https://twitter.com/gcfatuva" target="_blank" rel="noreferrer" className="social">
-          <FaTwitter/>
-        </a>
-        <a href="https://www.youtube.com/channel/UC7-1yqo1ig1VSDeg8eQ-S9w" target="_blank" rel="noreferrer" className="social">
-          <FaYoutube/>
-        </a>
+      <div className="connectBody">
+        <div className="connectCard">
+          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScLxzEHctUpwNMDGAxDg6DXrKKMUW9ifLfVKvlIFkT_ar33lA/viewform?embedded=true" className="iframe" width="100%" height="600" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+        </div>
+        <Fade right delay="1000">
+          <div className="sideConnect">
+            <div className="socialMedia">
+              <p className='socialMediaText'>Find us on social media</p>
+              <div className="socialMediaIcons">
+                <a href="https://www.facebook.com/GCFatUVA" target="_blank" rel="noreferrer" className="social">
+                  <FaFacebook/>
+                </a>
+                <a href="https://www.instagram.com/gcfuva/" target="_blank" rel="noreferrer" className="social">
+                  <FaInstagram />
+                </a>
+              </div>
+              <div className="socialMediaIcons">
+                <a href="https://twitter.com/gcfatuva" target="_blank" rel="noreferrer" className="social">
+                  <FaTwitter/>
+                </a>
+                <a href="https://www.youtube.com/channel/UC7-1yqo1ig1VSDeg8eQ-S9w" target="_blank" rel="noreferrer" className="social">
+                  <FaYoutube/>
+                </a>
+              </div>
+            </div>
+            <div className="sideEmail">
+              <p className='emailText'>Email Us</p>
+              <div className="emailContent">
+                <a href="mailto:cr9ey@virginia.edu" className="emailIcon"><HiOutlineMail></HiOutlineMail></a>
+                cr9ey@virginia.edu
+              </div>
+            </div>
+          </div>
+        </Fade>
       </div>
     </div>
   );

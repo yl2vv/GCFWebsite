839c01a055fddfc8f86f8dd9275d2473c278c75f
Sung Joon <sp3bk@virginia.edu>
2021-01-18 23:24:15 -0500

    nav bar underline ui

--- gcf_website/src/components/navbar.js
+++ gcf_website/src/components/navbar.js
@@ -3,64 +3,66 @@ import { NavLink } from "react-router-dom";
 import "./navbar.css";
 
 function Nav() {
-
   const [navbar, setNavbar] = useState(false);
 
   const changeBackground = () => {
-    if(window.scrollY >= (window.screen.height /15)) {
+    if (window.scrollY >= window.screen.height / 15) {
       setNavbar(true);
-    }
-    else {
+    } else {
       setNavbar(false);
     }
-  }
+  };
 
-  window.addEventListener('scroll', changeBackground);
+  window.addEventListener("scroll", changeBackground);
 
   return (
-    <nav className={navbar ? 'nav active' : 'nav'}>
+    <nav className={navbar ? "nav active" : "nav"}>
       <div className="leftCorner">
-          <NavLink
-          exact={true}
-          className="link"
-          to="/"
-          >
-            <div className='logo'></div>
-          </NavLink>
+        <NavLink exact={true} className="link" to="/">
+          <div className="logo"></div>
+        </NavLink>
       </div>
       <u1 className="nav-links">
         <NavLink className="link" activeClassName="link-active" to="/about">
-          <p>About</p>
+          <div className="about_container">
+            <p className="navtext">About</p>
+          </div>
         </NavLink>
         <NavLink
           className="link"
           activeClassName="link-active"
           to="/staff_and_leadership"
         >
-          <p>Staff / Leadership</p>
+          <div className="staff_container">
+            <p className="navtext">Staff / Leadership</p>
+          </div>
         </NavLink>
         <NavLink
           className="link"
           activeClassName="link-active"
           to="/ministries"
         >
-          <p>Ministries</p>
+          <div className="ministry_container">
+            <p className="navtext">Ministries</p>
+          </div>
         </NavLink>
         <NavLink className="link" activeClassName="link-active" to="/events">
-          <p>Events</p>
+          <div className="event_container">
+            <p className="navtext">Events</p>
+          </div>
         </NavLink>
-        <NavLink
-          className="link"
-          activeClassName="link-active"
-          to="/support"
-        >
-          <p>Support</p>
+        <NavLink className="link" activeClassName="link-active" to="/support">
+          <div className="support_container">
+            <p className="navtext">Support</p>
+          </div>
         </NavLink>
         {/* <NavLink className="link" activeClassName="link-active" to="/pictures">
           <p>Pictures</p>
         </NavLink> */}
         <NavLink className="link" activeClassName="link-active" to="/connect">
-          <p>Connect</p>
+          <div className="connect_container">
+            <p className="navtext">Connect</p>
+          </div>
         </NavLink>
       </u1>
     </nav>

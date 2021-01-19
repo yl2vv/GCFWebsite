f47852a90d5b1dfb64e71db18d657d5edba80713
Sung Joon <sp3bk@virginia.edu>
2021-01-18 23:05:44 -0500

    started support, ui change, ministry banner and ui

--- gcf_website/src/components/ministries.js
+++ gcf_website/src/components/ministries.js
@@ -1,16 +1,16 @@
 import React from "react";
-import Paper from '@material-ui/core/Paper';
-import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Avatar from '@material-ui/core/Avatar';
-import Typography from '@material-ui/core/Typography';
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
 
 import "./ministries.css";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    overflow: 'hidden',
+    overflow: "hidden",
     padding: theme.spacing(0),
   },
   paper: {
@@ -23,58 +23,97 @@ const useStyles = makeStyles((theme) => ({
 const message = `Truncation should be conditionally applicable on this long line of text
  as this is a much longer line than what the container can support. `;
 
-
 function Ministries() {
   const classes = useStyles();
 
   return (
     <div>
-      {/* <p className="ministries_title">Ministries</p> */}
+      <div className="banner ministries">
+        <p className="ministries_title">Ministries</p>
+      </div>
       <div class="row">
-        <div class="column"><p>Inreach</p>
+        <div class="column">
+          <p>Inreach</p>
           <div className={classes.root}>
             <Grid item xs wrap="nowrap" spacing={2} className={classes.paper}>
               <div className="post me">
-                <div className="me height"/>
+                <div className="me height" />
                 <div class="post-s">
                   <div className="text">Live Love</div>
-                  <div className="text">Homeless Outreach Ministry</div>
-                    <div className="text">Led By Jennifer Cheung and Philip Song</div>
+                  <div className="type">Homeless Outreach Ministry</div>
+                  <p className="snippet">
+                    What we do: we host events where UVA students can send in
+                    any questions about God, faith, or Christianity. We receive
+                    the questions and discuss them amongst ourselves in an
+                    apartment, referring to the Bible as our source of truth,
+                    and then go in teams to deliver a grilled cheese sandwich
+                    and an answer to the student who asked the question.
+                    <br />
+                    Purpose / Vision: We are an outreach ministry that seeks to
+                    spread God’s love and make His name known through sharing
+                    food and having discussions with the greater UVA community!
+                    <br />
+                    When we meet: meeting tri-weekly on thursdays at 9:15PM -
+                    12AM (can leave before that too)
+                    <br />
+                    Why someone should join: people should join to fulfill our
+                    calling to spread His word and His love, and to have an
+                    opportunity to have fruitful discussions and conversations
+                    with GCF/Cru people and with the UVA community.
+                    <br />
+                    Contact info: Phil Choi (psc3yh), Carmen Mew &nbsp(cem6dp)
+                  </p>
                 </div>
               </div>
             </Grid>
             <Grid item xs wrap="nowrap" spacing={2} className={classes.paper}>
               <div className="post me">
-                <div className="me height"/>
+                <div className="me height" />
                 <div class="post-s">
                   <div className="text">Live Love</div>
-                  <div className="text">Homeless Outreach Ministry</div>
-                    <div className="text">Led By Jennifer Cheung and Philip Song</div>
+                  <div className="type">Homeless Outreach Ministry</div>
+                  <div className="leaders">
+                    Led by Jennifer Cheung & Philip Song
+                  </div>
+                  <div className="snippet">
+                    Short description of ministry goal, activity and schedule
+                  </div>
                 </div>
               </div>
             </Grid>
           </div>
         </div>
-        </div>
-        <div class="column"><p>Outreach</p>
+      </div>
+      <div class="column">
+        <p>Outreach</p>
         <div className={classes.root}>
           <Grid item xs wrap="nowrap" spacing={2} className={classes.paper}>
             <div className="post me">
-              <div className="me height"/>
+              <div className="me height" />
               <div class="post-s">
                 <div className="text">Live Love</div>
-                <div className="text">Homeless Outreach Ministry</div>
-                  <div className="text">Led By Jennifer Cheung and Philip Song</div>
+                <div className="type">Homeless Outreach Ministry</div>
+                <div className="leaders">
+                  Led by Jennifer Cheung & Philip Song
+                </div>
+                <div className="snippet">
+                  Short description of ministry goal, activity and schedule
+                </div>
               </div>
             </div>
           </Grid>
           <Grid item xs wrap="nowrap" spacing={2} className={classes.paper}>
             <div className="post me">
-              <div className="me height"/>
+              <div className="me height" />
               <div class="post-s">
                 <div className="text">Live Love</div>
-                <div className="text">Homeless Outreach Ministry</div>
-                  <div className="text">Led By Jennifer Cheung and Philip Song</div>
+                <div className="type">Homeless Outreach Ministry</div>
+                <div className="leaders">
+                  Led by Jennifer Cheung & Philip Song
+                </div>
+                <div className="snippet">
+                  Short description of ministry goal, activity and schedule
+                </div>
               </div>
             </div>
           </Grid>
--- gcf_website/src/components/support.js
+++ gcf_website/src/components/support.js
@@ -4,9 +4,26 @@ import "./support.css";
 function Support() {
   return (
     <div className="support">
-      <div className='banner support'>
-       <p className="support_title">How To Support</p>
+      <div className="banner support">
+        <p className="support_title">How To Support</p>
       </div>
+      <p className="mission">
+        Our ministry is made possible through faithful prayer and financial
+        supporters. Please contact us if you are interested in joining this
+        cause of helping young men and women to embrace and follow the way of
+        God's love and truth during the important years of their life.
+      </p>
+      <div className="donation">Donate Here</div>
+      <p className="mission">
+        Servants Ministry is a non-profit tax-exempt 501(c)(3) organization. All
+        donations are tax deductible to the extent allowed by law. Donors will
+        receive an annual contribution statement by the end of January for
+        donations made during the previous year. For questions, please contact
+        Justin Gross at
+        <div className="contact_container">
+          <p className="contact">jgross@servantsministry.org.</p>
+        </div>
+      </p>
     </div>
   );
 }

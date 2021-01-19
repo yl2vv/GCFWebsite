79fe206f045a37616a0cdb2a736ec2675daa98f6
Sung Joon <sp3bk@virginia.edu>
2021-01-18 23:56:53 -0500

    support link

--- gcf_website/src/components/support.js
+++ gcf_website/src/components/support.js
@@ -13,7 +13,14 @@ function Support() {
         cause of helping young men and women to embrace and follow the way of
         God's love and truth during the important years of their life.
       </p>
-      <div className="donation">Donate Here</div>
+      <div className="donation_container">
+        <a
+          className="donation_link"
+          href="https://www.paypal.com/donate?token=0sTslCiDx-Hq38afiiynb66SMJXgh0DZTLfuYczyI9rBsakIPaRI_sSIUKaJvstshmO_EWFM8ykiOZc6"
+        >
+          <p className="donation">DONATE HERE</p>
+        </a>
+      </div>
       <p className="mission">
         Servants Ministry is a non-profit tax-exempt 501(c)(3) organization. All
         donations are tax deductible to the extent allowed by law. Donors will

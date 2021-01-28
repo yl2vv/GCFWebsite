5b19b4fe0ab72eb054dcd714a7a8ec95a0254967
James <yl2vv@virginia.edu>
2021-01-28 00:13:45 -0500

    events page start

--- src/components/events.js
+++ src/components/events.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./events.css";
 
+
 function getDayOfWeek(date) {
   const dayOfWeek = new Date(date).getDay();    
   return isNaN(dayOfWeek) ? null : 
@@ -18,6 +19,7 @@ class Events extends React.Component {
         break;
       case "Wednesday":
         document.getElementById("Wednesday").setAttribute("style", "color: rgb(29,98,57);");
+
         break;
       case "Thursday":
         document.getElementById("Thursday").setAttribute("style", "color: rgb(29,98,57);");
@@ -31,7 +33,7 @@ class Events extends React.Component {
       default:
         document.getElementById("Sunday").setAttribute("color", "color: rgb(29,98,57);");
     }
-}
+  } 
 render() {
   return (
     <div className='events'>
@@ -39,43 +41,108 @@ render() {
        <p className="events_title">Events</p>
       </div>
       <div className='eventsContent'>
-        <div className="weekTitle">Weekly Meetings</div>
-         <div className="week">
-          <div className="square first">
-            <div className="day" id="Monday">Monday</div>
-            <div className="event"></div>
+      <div className='scheduleWeek'>
+          <div className='weekContainer'>
+            <div className="weekTitle">Recurring Events</div>
+            <div className="weekSubContainer">
+              <div className="day">
+                <div className="dayContainer" id='Monday'>MON</div>
+              </div>
+              <div className="event">
+                <div className="eventContainer">Morning Prayer @8:00AM</div>
+              </div>
+            </div>
+            <div className="weekSubContainer">
+              <div className="day">
+                <div className="dayContainer" id='Tuesday'>TUE</div>
+              </div>
+              <div className="event">
+                <div className="eventContainer">Morning Prayer @8:00AM</div>
+              </div>
+            </div>
+            <div className="weekSubContainer">
+              <div className="day">
+                <div className="dayContainer" id='Wednesday'>WED</div>
+              </div>
+              <div className="event">
+                <div className="eventContainer">Morning Prayer @8:00AM <br/> Prayer Meeting @9:00PM</div>
+              </div>
+            </div>
+            <div className="weekSubContainer">
+              <div className="day">
+                <div className="dayContainer" id='Thursday'>THU</div>
+              </div>
+              <div className="event">
+                <div className="eventContainer">Morning Prayer @8:00AM</div>
+              </div>
+            </div>
+            <div className="weekSubContainer">
+              <div className="day">
+                <div className="dayContainer" id='Friday'>FRI</div>
+              </div>
+              <div className="event">
+                <div className="eventContainer">Morning Prayer @8:00AM <br/> Large Group @7:15PM</div>
+              </div>
+            </div>
+            <div className="weekSubContainer">
+              <div className="day">
+                <div className="dayContainer" id='Saturday'>SAT</div>
+              </div>
+              <div className="event">
+                <div className="eventContainer">-</div>
+              </div>
+            </div>
+            <div className="weekSubContainer">
+              <div className="day">
+                <div className="dayContainer" id='Sunday'>SUN</div>
+              </div>
+              <div className="event">
+                <div className="eventContainer">Sunday Chapel @11:00AM</div>
+              </div>
+            </div>
+
           </div>
-          <div className="square">
-            <div className="day" id="Tuesday">Tuesday</div>
-            <div className="event"></div>
+          
+        </div>
+        <div className='specialEvents'>
+          <div className='retreats'>
+            <div className='retreat'>
+              <div className='retreatTitle'>
+                Servants Retreat
+              </div>
+              <div className='retreatPic Servants'></div>
+            </div>
+
+            <div className='retreat'>
+              <div className='retreatTitle'>
+                Men's Retreat
+              </div>
+              <div className='retreatPic Mens'></div>
+            </div>
+
+            <div className='retreat'>
+              <div className='retreatTitle'>
+                Women's Retreat
+              </div>
+              <div className='retreatPic Womens'></div>
+            </div>
+
+            <div className='retreat'>
+              <div className='retreatTitle'>
+                Spring Retreat
+              </div>
+              <div className='retreatPic Spring'></div>
+            </div>
+
+            <div className='retreat'>
+              <div className='retreatTitle'>
+                Harvest Retreat
+              </div>
+              <div className='retreatPic Harvest'></div>
+            </div>
           </div>
-          <div className="square">
-            <div className="day" id="Wednesday">Wednesday</div>
-            <div className="event">Prayer Meeting @9:00PM</div>
-          </div>
-          <div className="square">
-            <div className="day" id="Thursday">Thursday</div>
-            <div className="event"></div>
-          </div>
-          <div className="square">
-           <div className="day" id="Friday">Friday</div>
-            <div className="event"> Large Group @7:30PM</div>
-          </div>
-          <div className="square">
-            <div className="day" id="Saturday">Saturday</div>
-            <div className="event"></div>
-          </div>
-          <div className="square">
-            <div className="day" id="Sunday">Sunday</div>
-            <div className="event"> Sunday Chapel @11:00AM</div>
-          </div>
-         </div>
-         {/* <div className='retreat'>
-           retreat
-         </div>
-         <div>
-           pictures?
-         </div> */}
+        </div>
+        
        </div>
     </div>
   );

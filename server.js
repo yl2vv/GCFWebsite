75baad9f2f5efe89b4515e7b083d4ed17504fca9
James <yl2vv@virginia.edu>
2021-02-15 18:43:44 -0500

    server change

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-// const express = require('express');
-// const favicon = require('express-favicon');
-// const path = require('path');
-// const port = process.env.PORT || 8080;
-// const app = express();
-// app.use(favicon(__dirname + '/build/favicon.ico'));
-// // the __dirname is the current directory from where the script is running
-// app.use(express.static(__dirname));
-// app.use(express.static(path.join(__dirname, 'build')));
-// app.get('/ping', function (req, res) {
-//  return res.send('pong');
-// });
-// app.get('/*', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
-// app.listen(port);
-var express = require('express');
-var app = express();
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(port);
+// var express = require('express');
+// var app = express();
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join('build', 'index.html'));
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('build'));
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join('build', 'index.html'));
+//   });
+// }
 
-app.use(express.static(__dirname + '/'));
-app.listen(process.env.PORT || 8080);
+// app.use(express.static(__dirname + '/'));
+// app.listen(process.env.PORT || 8080);
